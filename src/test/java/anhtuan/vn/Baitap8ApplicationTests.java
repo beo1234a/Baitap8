dbac0e10c8d6779b4f985c8a249a570b1dc5690a
beo1234a <tuantu612006@gmail.com>
2026-09-21 08:13:47 +0700

    Create Baitap8 Spring Boot project

---
+++ src/test/java/anhtuan/vn/Baitap8ApplicationTests.java
@@ -0,0 +1,13 @@
+package anhtuan.vn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baitap8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
